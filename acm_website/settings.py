2151aad63a8faee618e26446ff21f6d6eda09ff4
basithalikp <sithukp50@gmail.com>
2025-12-12 19:16:03 +0530

    Fixed some errors in deployement

--- acm_website/settings.py
+++ acm_website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'a-default-secret-key-for-local-dev')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['nssce.acm.org']
+ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'acm_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [BASE_DIR / 'acm_chapter/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
